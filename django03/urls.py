84606eeb879841e1690a4e3e61a084740bd9cd61
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-12-16 12:24:06 -0300

    Criando as rotas

--- django03/urls.py
+++ django03/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', include('crud.urls')),
+    path('crud/', include('crud.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url = '/static/images/favicon.ico'))
 ]
