89ad0ad4a8265e7c59c7aec81f4f0681165680ec
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-12-16 00:10:22 -0300

    Arquivos diversos

---
+++ venvdjango03/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rogerio.martins\PycharmProjects\udemy\django03\venvdjango03\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
